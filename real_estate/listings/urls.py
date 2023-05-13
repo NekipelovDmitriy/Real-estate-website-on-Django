15ec29ee94166999fc23ae9a8f60293404df2bee
iseodevil <iseodevil@gmail.com>
2023-05-13 23:46:10 +0800

    добавлен crud

--- real_estate/listings/urls.py
+++ real_estate/listings/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.listing_list, name='listings'),
+    path('listings/<pk>/', views.listing_retrieve , name='listing'),
+    path('listing/<pk>/edit', views.listing_update, name='update'),
+    path('listing/<pk>/delete', views.listing_delete, name='delete'),
+    path('create', views.listing_create, name='listing_create'),    
 ]
